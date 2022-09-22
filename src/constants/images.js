0060742079eb4b1e0f9e7177f62c77efe860fb07
m0-SA <suneetm0@gmail.com>
2022-09-22 21:25:05 +0100

    CSS Fixes

--- src/constants/images.js
+++ src/constants/images.js
@@ -1,6 +1,7 @@
 import knife from "../assets/knife.png";
 import overlaybg from "../assets/overlaybg.png";
 import spoon from "../assets/spoon.svg";
+import spoonflipped from "../assets/spoonflipped.svg";
 import cake from "../assets/cake.png";
 import coffee from "../assets/coffee.png";
 import coffee2 from "../assets/coffee2.jpg";
@@ -10,6 +11,7 @@ export default {
   knife,
   overlaybg,
   spoon,
+  spoonflipped,
   cake,
   coffee,
   coffee2,
--- src/container/AboutUs/AboutUs.jsx
+++ src/container/AboutUs/AboutUs.jsx
@@ -1,17 +1,30 @@
-import React from 'react';
+import React from "react";
 
-import { images } from '../../constants';
-import './AboutUs.css';
+import { images } from "../../constants";
+import "./AboutUs.css";
 
 const AboutUs = () => (
-  <div className="app__aboutus app__bg flex__center section__padding" id="about">
-
+  <div
+    className="app__aboutus app__bg flex__center section__padding"
+    id="about"
+  >
     <div className="app__aboutus-content flex__center">
       <div className="app__aboutus-content_about">
         <h1 className="headtext__cormorant">About Us</h1>
         <img src={images.spoon} alt="about_spoon" className="spoon__img" />
-        <p className="p__opensans">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quis pharetra adipiscing ultrices vulputate posuere tristique. In sed odio nec aliquet eu proin mauris et.</p>
-        <button type="button" className="custom__button" id="button">Know More</button>
+        <p className="p__opensans">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus sit
+          amet arcu malesuada, dignissim mi at, convallis erat. Praesent sed
+          ligula vel tellus sodales malesuada vel eu tellus. Quisque tempor
+          venenatis dapibus. Etiam ultricies arcu lacus, sit amet imperdiet est
+          sollicitudin id. Mauris vel elit ex. Etiam felis felis, bibendum id
+          dapibus vel, sagittis et mi. In auctor varius lobortis. Aenean
+          vestibulum nulla nec imperdiet pretium. Etiam tincidunt erat ligula,
+          nec hendrerit sapien iaculis sed. Maecenas nec scelerisque nibh. Proin
+          sagittis odio vitae felis volutpat hendrerit. Orci varius natoque
+          penatibus et magnis dis parturient montes, nascetur ridiculus mus.
+          Morbi at.
+        </p>
       </div>
 
       <div className="app__aboutus-content_knife flex__center">
@@ -20,9 +33,24 @@ const AboutUs = () => (
 
       <div className="app__aboutus-content_history">
         <h1 className="headtext__cormorant">Our History</h1>
-        <img src={images.spoon} alt="about_spoon" className="spoon__img" />
-        <p className="p__opensans">Adipiscing tempus ullamcorper lobortis odio tellus arcu volutpat. Risus placerat morbi volutpat habitasse interdum mi aliquam In sed odio nec aliquet.</p>
-        <button type="button" className="custom__button" id="button">Know More</button>
+        <img
+          src={images.spoonflipped}
+          alt="about_spoon"
+          className="spoon__img"
+        />
+        <p className="p__opensans">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus sit
+          amet arcu malesuada, dignissim mi at, convallis erat. Praesent sed
+          ligula vel tellus sodales malesuada vel eu tellus. Quisque tempor
+          venenatis dapibus. Etiam ultricies arcu lacus, sit amet imperdiet est
+          sollicitudin id. Mauris vel elit ex. Etiam felis felis, bibendum id
+          dapibus vel, sagittis et mi. In auctor varius lobortis. Aenean
+          vestibulum nulla nec imperdiet pretium. Etiam tincidunt erat ligula,
+          nec hendrerit sapien iaculis sed. Maecenas nec scelerisque nibh. Proin
+          sagittis odio vitae felis volutpat hendrerit. Orci varius natoque
+          penatibus et magnis dis parturient montes, nascetur ridiculus mus.
+          Morbi at.
+        </p>
       </div>
     </div>
   </div>
